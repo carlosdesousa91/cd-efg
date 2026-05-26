97d481d84d9726e8ab7ac52ea57dcc6f1437d227
carlosdesousa91 <31635606+carlosdesousa91@users.noreply.github.com>
2026-05-26 19:58:14 -0300

    Update estoque.py

--- projeto-estoque/estoque.py
+++ projeto-estoque/estoque.py
@@ -2,20 +2,44 @@ from data_base import salvar_dados, carregar_dados
 
 def listar_produtos():
     lista_de_produtos = carregar_dados()
-    print(lista_de_produtos)
+    #print(lista_de_produtos)
+    for produto in lista_de_produtos:
+        print("######## Lista de produtos ########")
+        print("Código: ", produto["codigo"])
+        print("Nome: ", produto["nome"])
+        print("Quantidade: ", produto["quantidade"])
+        print("Preço: ", produto["preco"])
+        print("------------------------------------")
 
 def cadastrar_produto():
-    codigo = input("digite o código do produto ")
+    
+    # Confere se o código já está em uso.
+    todos_os_dados_dos_produtos = carregar_dados()
+    lista_de_codigos_dos_produtos = []
+    for produto_um_por_um in todos_os_dados_dos_produtos:
+        lista_de_codigos_dos_produtos.append(produto_um_por_um['codigo'])
+    
+    #codigo = input("digite o código do produto ")
+    # Cria o código automaticamente.
+    quantidade_de_produtos = len(carregar_dados())
+    codigo = quantidade_de_produtos + 1 
+
+    
+    #Recebe os campos digitados pelo usuário.
     nome = input("digite o nome do produto ")
     quantidade = input("digite a quantidade de produtos ")
     preco = input("digite o preço do produto ")
-    # print("produto cadastrado com sucesso: ", codigo, nome, quantidade, preco)
-    produtos = [{
-        "codigo": codigo,
-        "nome": nome,
-        "quantidade": quantidade,
-        "preco": preco
-    }]
+
+    produtos = carregar_dados()
+    produtos.append(
+        {
+            "codigo": codigo,
+            "nome": nome,
+            "quantidade": quantidade,
+            "preco": preco
+        }
+    )    
+    
     salvar_dados(produtos)
     print("produto cadastrado com sucesso!")
 
